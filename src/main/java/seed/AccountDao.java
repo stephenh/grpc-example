e3b5329e19628351265fdc7f74ff71536234d9bb
Stephen Haberman <stephen.haberman@gmail.com>
2018-05-17 11:02:20 -0500

    Forgot not nulls, rename user->account.

--- src/main/java/seed/AccountDao.java
+++ src/main/java/seed/AccountDao.java
@@ -9,15 +9,15 @@ import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 public interface AccountDao {
 
-  @SqlUpdate("CREATE TABLE account (id INT AUTO_INCREMENT, status INT, name VARCHAR(200), address VARCHAR(200), ssn VARCHAR(200))")
+  @SqlUpdate("CREATE TABLE account (id INT AUTO_INCREMENT, status INT NOT NULL, name VARCHAR(200) NOT NULL, address VARCHAR(200) NOT NULL, ssn VARCHAR(200) NOT NULL)")
   void createTable();
 
   @SqlUpdate("INSERT INTO account (name, status, address, ssn) VALUES (:name, :statusValue, :address, :ssn)")
   @GetGeneratedKeys
-  long insert(@BindBean Account user);
+  long insert(@BindBean Account account);
 
   @SqlUpdate("UPDATE account SET name = :name, status = :statusValue, address = :address, ssn = :ssn WHERE id = :id")
-  void update(@BindBean Account user);
+  void update(@BindBean Account account);
 
   @SqlQuery("SELECT * FROM account WHERE id = ?")
   Account read(long id);
