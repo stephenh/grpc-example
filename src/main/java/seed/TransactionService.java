545400a3be5f2ddcc22f7794c5537b28e61a53a4
Stephen Haberman <stephen.haberman@gmail.com>
2018-05-17 22:07:37 -0500

    Move sourceBalance check to early return so we can dedent a big chunk.

--- src/main/java/seed/TransactionService.java
+++ src/main/java/seed/TransactionService.java
@@ -34,61 +34,61 @@ public class TransactionService extends TransactionServiceImplBase {
 
       // TODO Add some sanity checks to sourceBalance, e.g. should be positive, should probably not be $1 billion
       long sourceBalance = dao.balance(req.getSourceAccountId());
-      if (sourceBalance >= req.getAmountInCents()) {
-        // TODO use a clock
-        long now = System.currentTimeMillis();
-        // make two new transactions; currently we don't tie them back to the same source event
-        dao.insert(
-          Transaction
-            .newBuilder()
-            .setAccountId(req.getSourceAccountId())
-            .setAmountInCents(req.getAmountInCents() * -1)
-            .setTimestampInMillis(now)
-            // TODO: I think "" is allowed, which we should probably reject
-            .setDescription(req.getDescription())
-            .build());
-        dao.insert(
-          Transaction
-            .newBuilder()
-            .setAccountId(req.getDestinationAccountId())
-            .setAmountInCents(req.getAmountInCents())
-            .setTimestampInMillis(now)
-            .setDescription(req.getDescription())
-            .build());
-
-        // We're in a transaction but technically should throb something on the
-        // account to keep concurrent transfers from over-drawing the account.
-        //
-        // AFAICT not even SERIALIZABLE transaction isolation would prevent over-
-        // draws with the current "read balance + insert two rows" approach. This
-        // is because this timeline is possible:
-        //
-        // txn1: queries transaction where accountId = 1, gets $10
-        // txn2: queries transaction where accountId = 1, gets $10
-        // txn1: adds two new transaction rows, one for accountId = -10
-        // txn2: adds two new transaction rows, one for accountId = -10
-        //
-        // None of the usual suspects (dirty read, non-repeatable read, or phantom read)
-        // have been violated here because we have not re-issued a read.
-        //
-        // I suppose that would be a way to do it, instead of bumping an op-lock
-        // on the account row (which would cause a conflict), we could re-read the
-        // balance and ensure it matches what we expect. This
-        // would trigger phantom read detection.
-        //
-        // Sorry, I don't have a unit test for this scenario. :-)
-
-        long newSourceBalance = dao.balance(req.getSourceAccountId());
-        if (newSourceBalance != sourceBalance - req.getAmountInCents()) {
-          // In theory we shouldn't get here, because it means we had a phantom read,
-          // which the database should have already detected/failed on
-          throw new RuntimeException("Potential overdraw detected");
-        }
-
-        return TransferResponse.newBuilder().setSuccess(true).build();
-      } else {
+      if (sourceBalance < req.getAmountInCents()) {
         return TransferResponse.newBuilder().setSuccess(false).addErrors("Insufficient funds").build();
       }
+
+      // TODO use a clock
+      long now = System.currentTimeMillis();
+      // make two new transactions; currently we don't tie them back to the same source event
+      dao.insert(
+        Transaction
+          .newBuilder()
+          .setAccountId(req.getSourceAccountId())
+          .setAmountInCents(req.getAmountInCents() * -1)
+          .setTimestampInMillis(now)
+          // TODO: I think "" is allowed, which we should probably reject
+          .setDescription(req.getDescription())
+          .build());
+      dao.insert(
+        Transaction
+          .newBuilder()
+          .setAccountId(req.getDestinationAccountId())
+          .setAmountInCents(req.getAmountInCents())
+          .setTimestampInMillis(now)
+          .setDescription(req.getDescription())
+          .build());
+
+      // We're in a transaction but technically should throb something on the
+      // account to keep concurrent transfers from over-drawing the account.
+      //
+      // AFAICT not even SERIALIZABLE transaction isolation would prevent over-
+      // draws with the current "read balance + insert two rows" approach. This
+      // is because this timeline is possible:
+      //
+      // txn1: queries transaction where accountId = 1, gets $10
+      // txn2: queries transaction where accountId = 1, gets $10
+      // txn1: adds two new transaction rows, one for accountId = -10
+      // txn2: adds two new transaction rows, one for accountId = -10
+      //
+      // None of the usual suspects (dirty read, non-repeatable read, or phantom read)
+      // have been violated here because we have not re-issued a read.
+      //
+      // I suppose that would be a way to do it, instead of bumping an op-lock
+      // on the account row (which would cause a conflict), we could re-read the
+      // balance and ensure it matches what we expect. This
+      // would trigger phantom read detection.
+      //
+      // Sorry, I don't have a unit test for this scenario. :-)
+
+      long newSourceBalance = dao.balance(req.getSourceAccountId());
+      if (newSourceBalance != sourceBalance - req.getAmountInCents()) {
+        // In theory we shouldn't get here, because it means we had a phantom read,
+        // which the database should have already detected/failed on
+        throw new RuntimeException("Potential overdraw detected");
+      }
+
+      return TransferResponse.newBuilder().setSuccess(true).build();
     });
     // TODO Add try/catch in case withTransaction throws
     res.onNext(response);
